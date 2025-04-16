a4e839f028b9cc9ece7b8935580b4f57cb44c826
zaicouto <marloncouto719@gmail.com>
2025-04-16 13:13:23 -0300

    Refactor code style and improve consistency across components and pages
    - Updated string quotes from single to double in various files for consistency.
    - Reformatted code for better readability, including indentation and spacing adjustments.
    - Enhanced the structure of HTML elements in multiple components and pages.
    - Improved the organization of CSS styles in global.css for better maintainability.
    - Ensured consistent use of type definitions in TypeScript files.
    - Updated blog post metadata to use double quotes for titles and descriptions.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog do Zai';
-export const SITE_DESCRIPTION = 'Tudo sobre programação: artigos, tutoriais e notícias';
+export const SITE_TITLE = "Blog do Zai";
+export const SITE_DESCRIPTION =
+  "Tudo sobre programação: artigos, tutoriais e notícias";
