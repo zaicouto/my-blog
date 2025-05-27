32ee0f23bb5791ebe4960c8b3d1ef555349f69ee
Ozair Couto <marloncouto719@gmail.com>
2025-05-26 22:40:08 -0300

    Update SITE_TITLE to reflect the correct blog name

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Blog do Zai";
+export const SITE_TITLE = "Código Ninja";
 export const SITE_DESCRIPTION =
   "Tudo sobre programação: artigos, tutoriais e notícias";
